741b4cd2d2d979a858073ab464820083760856dc
cuongtx <cuongtx@natajsc.com.vn>
2020-07-08 15:38:42 +0700

    add lab 1 basic js

---
+++ cuongtxps07744_lab1_jsbasic/exp1/lab1_1_2.js
@@ -0,0 +1,9 @@
+function fillInfo() {
+    var name = "To Xuan Cuong";
+    var code = "PS07744";
+    var dob = "07 - 10 - 1998";
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("code").innerHTML = code;
+    document.getElementById("dob").innerHTML = dob;
+}
+
