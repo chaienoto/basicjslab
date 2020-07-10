4cfc22b4294b0d7f5f113ae185a559b756e102fc
cuongtx <cuongtx@natajsc.com.vn>
2020-07-10 17:04:15 +0700

    compelete lab2

---
+++ cuongtxps07744_lab2_jsbasic/exp3/lab1_3.js
@@ -0,0 +1,16 @@
+function calculation(){
+    var h = 3 ;
+    var w = 4 ;
+    var v = 2*(h+w) ;
+    var s = h*w
+    document.getElementById("h").innerHTML = h;
+    document.getElementById("w").innerHTML = w;
+    document.getElementById("v").innerHTML = v;
+    document.getElementById("s").innerHTML = s;
+    console.log(h)
+    console.log(w)
+    console.log(v)
+    console.log(s)
+
+   
+}
